670abc07e21ef651fb443e2f4f9d3d15b67a490d
Corvo <jodamaru@gmail.com>
2014-09-29 03:17:26 -0400

    created partials, still have old code commented

--- app/controllers/updates_controller.rb
+++ app/controllers/updates_controller.rb
@@ -1,2 +1,21 @@
 class UpdatesController < ApplicationController
+
+	def index
+		@updates = Update.all	
+	end
+
+	def show
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def update
+	end
+
+	def destroy
+	end
 end
